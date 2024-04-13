a4633d63a0318a7ed1bff08ba1e1d1901dc93b3d
pisa-kun <pisa0401mst@gmail.com>
2024-04-13 22:56:50 +0900

    Initial commit

---
+++ test/cdk-fargate-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkFargateSample from '../lib/cdk-fargate-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-fargate-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkFargateSample.CdkFargateSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
